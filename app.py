95b548e3b648b4c5930592da897112bd346ca942
Madhulika2408 <madhulikabogam5@gmail.com>
2026-02-20 11:58:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from sklearn.datasets import load_iris
 data = load_iris()
-
 from sklearn.ensemble import RandomForestClassifier
 model = RandomForestClassifier()
 x = data.data
